b97a90a4e949311ff0df9e19e8152a3a47401e14
Gajaa-Azhura <shinr433@gmail.com>
2025-03-20 10:20:09 +0530

    changes on file

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ let number1 = document.getElementById('num1');
 let number2 = document.getElementById('num2');
 
 function compare() {
-   
     document.getElementById('greater').innerHTML = '';
     document.getElementById('lesser').innerHTML = '';
     document.getElementById('equalto').innerHTML = '';
     document.getElementById('notequal').innerHTML = '';
 
-    
     let num1 = parseFloat(number1.value);
     let num2 = parseFloat(number2.value);
 
     if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById('notequal').innerHTML = 'both numbers are not equal.';
+        document.getElementById('notequal').innerHTML = 'Please enter valid numbers.';
     } else if (num1 > num2) {
         document.getElementById('greater').innerHTML = 'Number 1 is greater than Number 2';
     } else if (num1 < num2) {
